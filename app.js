7fdd64d767b98cb4e71ed2530832ed62ee24824d
Leonardo Vicentini <Leonardo.Vicentini@marconi.loc>
2018-10-03 16:13:07 +0200

    Update fine pomeriggio

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-var http = require('http');
-var conta=0;
-var server = http.createServer(function (request, response)
-     {   conta+=1; 
-         console.log('request starting...'+conta);    
-         // respond    
-         response.write('hello client!');    
-         response.end();
-    });
 
-server.listen(3000);
-console.log('Server running at http://127.0.0.1:3000/');
+var express = require("express");
+var path = require("path");
+var fs = require("fs");
+var app = express();
+app.use(function(req, res, next) {
+    console.log("Request IP: " + req.url);
+    console.log("Request date: " + new Date());
+});
+app.listen(3000, function() {
+    console.log("App started on port 3000");
+});
